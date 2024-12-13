6538e8608d3898ac9f97eecd67a686a43662ccad
d-cuzya <Admin@DESKTOP-T6OG5FF>
2024-12-13 14:28:08 +0300

    Добавьте файлы проекта.

---
+++ viewmodel/viewmodel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp2
+{
+    public partial class MainWindow
+    {
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sender.Content()
+        }
+    }
+   
+}
