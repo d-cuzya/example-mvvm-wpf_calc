7b8d2a99b5fe818059efbd84b2ece8e6bbcc6305
d-cuzya <Admin@DESKTOP-T6OG5FF>
2024-12-13 15:30:55 +0300

    sadasdsadsa

--- model/model.cs
+++ model/model.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
 
 namespace WpfApp2
 {
     public partial class MainWindow
     {
+        private void plus(Label a, Label b)
+        {
+            if (a.ToString() == "")
+            {
+                MessageBox.Show("Надо что нить написать!", "Error", MessageBoxButton.OK);
+            }
+        }
     }
 }
--- viewmodel/viewmodel.cs
+++ viewmodel/viewmodel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace WpfApp2
 {
@@ -11,7 +12,50 @@ namespace WpfApp2
     {
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            sender.Content()
+            switch (((Button)sender).Content.ToString())
+            {
+                // через command parameter wpf
+                // https://metanit.com/sharp/wpf/22.4.php
+                case "0":
+                    labelRes.Content += "0";
+                    break;
+                case "1":
+                    labelRes.Content += "1";
+                    break;
+                case "2":
+                    labelRes.Content += "2";
+                    break;
+                case "3":
+                    labelRes.Content += "3";
+                    break;
+                case "4":
+                    labelRes.Content += "4";
+                    break;
+                case "5":
+                    labelRes.Content += "5";
+                    break;
+                case "6":
+                    labelRes.Content += "6";
+                    break;
+                case "7":
+                    labelRes.Content += "7";
+                    break;
+                case "8":
+                    labelRes.Content += "8";
+                    break;
+                case "9":
+                    labelRes.Content += "9";
+                    break;
+                case "+":
+                    plus(labelRes, labelOld);
+                    break;
+                case "-":
+                    break;
+                case "*":
+                    break;
+                case "/":
+                    break;
+            }
         }
     }
    
